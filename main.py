59edd51989cf1a2cb8c4d5e46ad6afca57d474f7
dpnetca <git@dpnet.ca>
2021-04-03 11:20:39 -0600

    change database url environmental variable key name

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("FLASK_DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
